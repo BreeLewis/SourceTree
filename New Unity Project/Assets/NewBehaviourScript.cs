8a7c2eafda3a308b600d663d13f049143d06147e
unknown <10715468@CS514-09.ad.uvu.edu>
2015-09-02 08:52:40 -0600

    First Unity

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+		int myInt=5;
+	// Use this for initialization
+	void Start () 
+	{
+		myInt = MultiplyByTwo (myInt);
+		Debug.Log (myInt);
+	}
+	// Method: Multipy interger by two
+	int MultiplyByTwo(int number)
+		{
+		int ret;
+		ret = number * 2;
+		return ret;
+		}
+}
