2e2225f8097374769598c80881826e34f08a903d
unknown <10715468@CS514-10.ad.uvu.edu>
2015-10-19 08:54:23 -0600

    Loops Trial

---
+++ Loops/Assets/Scripts/DoWhile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoWhile : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+	bool shouldContinue= false;
+	 do
+	 {
+	 	print ("Hello World");
+	 }while(shouldContinue == true);
+	
+	}
+}
---
+++ Loops/Assets/Scripts/For.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class For : MonoBehaviour 
+{
+int enemies = 3;
+	void Start () 
+	{
+	 	for (int i = 0; i < enemies; i++)
+		 {
+			 Debug.Log ("Creating enemy number: " + i);
+	 	 }	
+	}
+}
