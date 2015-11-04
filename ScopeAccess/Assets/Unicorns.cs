8c7be2a06e4ad0470bd544f9c82bc680e88ba705
unknown <10715468@CS514-10.ad.uvu.edu>
2015-11-04 09:01:19 -0700

    Scope and Access

---
+++ ScopeAccess/Assets/Unicorns.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unicorns : MonoBehaviour 
+{
+	private int white = 2;
+	private int black = 4;
+	private Dragons myOtherClass;
+	public int herd= 8;
+	private int grey = 4;
+	private int brown = 4;
+	public int death;
+	
+	private void Unicorn () 
+	{
+	 herd = grey + brown;
+	 Debug.Log ("population:" +herd);
+	}
+	void start()
+	{
+	 myOtherClass = new Dragons();
+	 myOtherClass.food= 2;
+	 myOtherClass.Consume(herd);
+	}
+
+ 	public void Eaten (int noGrass) 
+	{
+	 int decline;
+	 decline = death * herd * noGrass;
+	 Debug.Log ("Death in the herd:" + decline);
+	}
+}
