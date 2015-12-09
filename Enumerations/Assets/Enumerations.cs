c8a653c7d43419ed7363bf3dfd0cef257f7fc04d
unknown <10715468@CS514-10.ad.uvu.edu>
2015-12-09 09:19:20 -0700

    Enumerations and Switch statements

---
+++ Enumerations/Assets/Enumerations.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumerations : MonoBehaviour 
+{	
+	enum Direction {North= 2, East= 10, South= 12, West= 20};
+	
+	void Start()
+	{
+		Direction myDirection;
+		myDirection = Direction.North;
+		myDirection = ReverseDirection (myDirection);
+	}
+	Direction ReverseDirection (Direction dir)
+		{
+			if(dir==Direction.North)
+				dir= Direction.South;
+			return dir;
+		}
+	}
+}
